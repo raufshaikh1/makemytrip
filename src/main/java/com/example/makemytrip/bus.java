b3fc996a1c6913699bf861f8ffa8ef40c7c07be7
unknown <shaikhrauf210@gmail.com>
2024-07-29 11:31:47 +0530

    souce code

--- src/main/java/com/example/makemytrip/bus.java
+++ src/main/java/com/example/makemytrip/bus.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class bus {
 
     @GetMapping("/bus")
-    public String getData() {return "Please book your bus at 10% discount at indigo" ;}
+    public String getData() {return "Please book your bus at 15% discount at indigo" ;}
 }
