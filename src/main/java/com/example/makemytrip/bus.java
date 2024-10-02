7a6e54257b068d8a14d841b4ebed83ada5b0953e
unknown <shaikhrauf210@gmail.com>
2024-10-02 12:01:57 +0530

    new feature added

---
+++ src/main/java/com/example/makemytrip/bus.java
@@ -0,0 +1,11 @@
+package com.example.makemytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class bus {
+
+    @GetMapping("/bus")
+    public String getData() {return "Please book your bus from IRTC with 15% discount" ;}
+}
